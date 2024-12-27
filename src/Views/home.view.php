6404cc904865b9518e2ce702c653fde5b4021937
DfrtJulien <de.freitas.julien78@gmail.com>
2024-12-27 19:25:33 +0100

    end of the home page

--- src/Views/home.view.php
+++ src/Views/home.view.php
@@ -1,1 +1,111 @@
-<h1>Hello</h1>
+<?php
+require_once(__DIR__ . "/partials/head.php");
+?>
+
+<section class="hero">
+    <div class="img-container img-container-header">
+        <img src="/public/img/header.jpg" alt="image pancackes">
+    </div>
+    <div class="hero-title">
+        <h1>Fructus</h1>
+        <p> Le meilleur endroit pour tous les passionées de patisserie.</p>
+    </div>
+</section>
+<section class="learnMore">
+    <div class="comunity">
+        <div class="img-container img-container-comunity">
+            <img src="/public/img/comunity.jpg" alt="chocolat cake">
+        </div>
+        <div class="comunityText">
+            <h4>Comunnauté</h4>
+            <p>Rejoignez nous passionnées de patisseries, de tout âge, professionnelle ou amateur.</p>
+            <div class="comunityLink">
+                <a href="">Inscrivez vous </a>
+                <i class="fa-solid fa-arrow-right"></i>
+            </div>
+        </div>
+    </div>
+    <div class="recipe">
+        <div class="recipeText">
+            <h4>Les meilleurs recettes</h4>
+            <p>Essayer nos meilleurs recettes créer par notre communauté !</p>
+            <div class="recipeLink">
+                <a href="">Essayer les recettes</a>
+                <i class="fa-solid fa-arrow-right"></i>
+            </div>
+        </div>
+        <div class="img-container img-container-recipe">
+            <img src="/public/img/recipe.jpg" alt="ceral bowl">
+        </div>
+    </div>
+    <div class="createYourRecipe">
+        <div class="img-container img-container-createYourRecipe">
+            <img src="/public/img/crepe.jpg" alt="chocolat crepe">
+        </div>
+        <div class="createYourRecipeText">
+            <h4>Créer vos propres recettes !</h4>
+            <p>Ajoutez vos propres recettes des plus simples au plus créatives. </p>
+        </div>
+    </div>
+</section>
+<section class="productFilterHome">
+    <div class="img-container">
+        <img src="/public/img/maccaron.jpg" alt="image of somes maccaron">
+    </div>
+    <div class="recipeFilterContainer">
+        <div class="recipeFlex">
+            <div class="recipeContainer">
+                <a href="">
+                    <div class="recipeFilter">
+                        <div class="filterLogoContainer">
+                            <img src="/public/img/burger.png" alt="burger icon" class="filterLogo">
+                        </div>
+                        <div>
+                            <h6>Crêpes</h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="recipeContainer">
+                <a href="">
+                    <div class="recipeFilter">
+                        <div class="filterLogoContainer">
+                            <img src="/public/img/burger.png" alt="burger icon" class="filterLogo">
+                        </div>
+                        <div>
+                            <h6>Crêpes</h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="recipeContainer">
+                <a href="">
+                    <div class="recipeFilter">
+                        <div class="filterLogoContainer">
+                            <img src="/public/img/burger.png" alt="burger icon" class="filterLogo">
+                        </div>
+                        <div>
+                            <h6>Crêpes</h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="recipeContainer">
+                <a href="">
+                    <div class="recipeFilter">
+                        <div class="filterLogoContainer">
+                            <img src="/public/img/burger.png" alt="burger icon" class="filterLogo">
+                        </div>
+                        <div>
+                            <h6>Crêpes</h6>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php
+include_once(__DIR__ . "/partials/footer.php");
